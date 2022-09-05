a9a9a88f5e45cfa6847ae7d136a9127d45448d4d
Gregor Adams <greg@pixelass.com>
2022-09-05 10:41:11 +0200

    Add user story 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+const endpoint = "https://fetch-me.vercel.app/api/shopping/items";
+
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const [data, setData] = useState(null);
+  const [activeItems, setActiveItems] = useState([]);
+
+  useEffect(() => {
+
+    async function fetchData() {
+      const response = await fetch(endpoint);
+      const json = await response.json();
+      setData(json.data);
+
+    }
+
+    fetchData();
+
+  }, []);
+
+  if (data === null) {
+    return <div>loading...</div>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Shopping List</h1>
+      <input type="search" value={searchQuery} onChange={(event) => {
+        setSearchQuery(event.target.value);
+      }} />
+      <h2>Active Items</h2>
+      <ul>
+        {activeItems.map(item => {
+          return (
+            <li key={item._id} >
+              {item.name.de}
+            </li>
+          )
+
+        })}
+      </ul>
+      <h2>Filtered Items</h2>
+      <ul>
+        {searchQuery === "" ? <h2>Please Enter a search term</h2> : data.filter(item => {
+          return item.name.de.toLowerCase().includes(searchQuery.toLowerCase());
+        }).map(item => {
+          return (
+            <li key={item._id} >
+              <button type="button" onClick={() => {
+                // Here
+                setActiveItems([...activeItems, item])
+                setSearchQuery("");
+              }}>
+                {item.name.de}
+              </button>
+            </li>
+          )
+        })}
+      </ul>
+      {data?.some(item => {
+        return item.name.de.toLowerCase().includes(searchQuery.toLowerCase());
+      }) ? null : "No matches"}
     </div>
   );
 }
